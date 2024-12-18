846761d10c6ae1bd21c7ea57a3dca4fe5284a102
Helena Hollænder <helena.hollaender@gmail.com>
2024-12-18 13:06:12 +0100

    import_lucide_icons

--- src/components/CategoryCom.vue
+++ src/components/CategoryCom.vue
@@ -1,6 +1,8 @@
 <script setup>
 import { defineProps } from 'vue';
 import ButtonCom from './ButtonCom.vue';
+import { Users } from 'lucide-vue-next';
+
 
 const props = defineProps({
     text: {
@@ -17,5 +19,6 @@ const props = defineProps({
 <template>
     <div>
        <ButtonCom variant="ghost" :text="text" />
+       <Users size="24" class="stroke-primarydark"/>
     </div>
 </template>
